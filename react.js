068d96ea37247d68d89c4349e4f2987b5dd235ff
Carl Durand <carldurand.student@careerdevs.com>
2019-05-13 16:57:51 +0000

    Pass a Callback as Props

--- react.js
+++ react.js
@@ -1,67 +1,54 @@
-import React from 'react';
-
-class App extends React.Component {
-  state={
-  name:"",
-  email:"",
-  phoneNumber:"",
-  submited: false 
+class MyApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
   }
-  
-  handleSubmit = () => {
-    this.setState({submitted:true})
-  }
-  
-  setName = event  => {
-    console.log(event.target.value);
+  handleChange(event) {
     this.setState({
-      name:event.target.value
+      inputValue: event.target.value
     });
   }
-  
-   setEmail = event  => {
-    console.log(event.target.value);
-    this.setState({
-      email:event.target.value
-    });
+  render() {
+    return (
+       <div>
+        { /* change code below this line */ }
+<GetInput input = {this.state.inputValue} handleChange = {this.handleChange}/>
+<RenderInput input = {this.state.inputValue}/>
+        { /* change code above this line */ }
+       </div>
+    );
   }
-  
-  setPhoneNumber = event  => {
-    console.log(event.target.value);
-    this.setState({
-      phoneNumber:event.target.value
-    });
-  }
-  
-  render(){
-  return (
-    <div className="App">
-      <h2>Social profile</h2>
-    <p>An app for your social media profiles</p>
-    { !this.state.submitted &&
-    <div>
-    <input onChange={this.setName} style={{"display":"block"}} placeholder="Name" />
-    <input onChange={this.setEmail} style={{"display":"block"}} placeholder="email" />
-    <input onChange={this.setPhoneNumber} style={{"display":"block"}} placeholder="phone number" />
-    <button onClick={this.handleSubmit }>Submit</button>
-    </div>}
-    
-    {this.state.submitted && 
-    
-    <div>
-    {"Your name is: " + this.state.name}
-    <br />
-    {"your email is: " + this.state.email}
-    <br />
-    {"your phone number is: " + this.state.phoneNumber}
-    
-    </div>}
-    
-    
-    </div>
-  );
-  }
-}
+};
 
+class GetInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Get Input:</h3>
+        <input
+          value={this.props.input}
+          onChange={this.props.handleChange}/>
+      </div>
+    );
+  }
+};
 
-export default App;
+class RenderInput extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <h3>Input Render:</h3>
+        <p>{this.props.input}</p>
+      </div>
+    );
+  }
+};
