527a55deeb111116ecaab18fa5cde23b8e4a48bb
Carl Durand <carldurand.student@careerdevs.com>
2019-05-15 16:45:24 +0000

    Reverse a String

--- data.js
+++ data.js
@@ -1,29 +1,13 @@
-let users = {
-  Alan: {
-    age: 27,
-    online: true
-  },
-  Jeff: {
-    age: 32,
-    online: true
-  },
-  Sarah: {
-    age: 48,
-    online: true
-  },
-  Ryan: {
-    age: 19,
-    online: true
-  }
-};
-
-function isEveryoneHere(obj) {
-  // change code below this line
-  if(users.hasOwnProperty('Alan','Jeff','Sarah','Ryan')) {
-    return true;
-  }
-  return false;
-  // change code above this line
+function reverseString(str) {
+  return str;
 }
 
-console.log(isEveryoneHere(users));
+reverseString("hello");
+
+//How to reverse a String()
+
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+reverseString("hello");
